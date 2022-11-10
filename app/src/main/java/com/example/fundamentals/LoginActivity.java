2f5b62a0353da304172793b28b57fff55b6679c6
Jordan Chimborazo <jordanchimborazo10@gmail.com>
2022-11-10 10:06:58 +0100

    cambio Fondo y logo Login

--- app/src/main/java/com/example/fundamentals/LoginActivity.java
+++ app/src/main/java/com/example/fundamentals/LoginActivity.java
@@ -2,7 +2,13 @@ package com.example.fundamentals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,7 +16,42 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //    Glide for loading girls
+        ImageView mGirl = findViewById(R.id.girl);
+
+
+        Glide.with(this)
+              .load("https://images.unsplash.com/photo-1549492423-400259a2e574?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=321&q=80")
+                //.load(R.drawable.girl)
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+
+                .centerCrop()
+//                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.fucsia_200)))
+//                .circleCrop()
+                .into(mGirl);
+
+        ImageView mlogo = findViewById(R.id.logo);
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1491349174775-aaafddd81942?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80")
+                //.load(R.drawable.girl)
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+
+                .circleCrop()
+//                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.fucsia_200)))
+//                .circleCrop()
+                .into(mlogo);
+
+
     }
 
-    public void openMain(view)
+
+    public void openSignup(View v) {
+        Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+        startActivity(intent);
+
+//       intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+    }
 }
